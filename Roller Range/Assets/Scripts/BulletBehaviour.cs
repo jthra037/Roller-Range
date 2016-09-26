68df2bb418e119538fcdb7fc1716a91958266252
jthra037 <its_thrasher@live.ca>
2016-09-26 18:29:40 -0400

    Add bullet prefab Add firing script Make guide boxes physics objects for testing.

---
+++ Roller Range/Assets/Scripts/BulletBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletBehaviour : MonoBehaviour {
+
+    Rigidbody rb;
+    float force = 100;
+
+	// Use this for initialization
+	void Start () {
+        rb = gameObject.GetComponent<Rigidbody>();
+        rb.AddForce(force * transform.forward, ForceMode.Impulse);
+	}
+}
