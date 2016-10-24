726b2b1d461cdbd1acf7aa8bd7d3a8e94991ce8b
jthra037 <its_thrasher@live.ca>
2016-10-24 18:58:56 -0400

    Add continuous fire option for player Add firing rate for player

--- Roller Range/Assets/Scripts/PlayerBehaviour.cs
+++ Roller Range/Assets/Scripts/PlayerBehaviour.cs
@@ -14,6 +14,10 @@ public class PlayerBehaviour : MonoBehaviour {
     public float minRot = -70;
     public float maxRot = 70;
 
+    [SerializeField]
+    private float fireRate = 1f;
+
+    private float lastShot;
     private Rigidbody rb;
     private Vector3 movement;
     private int layer;
@@ -22,16 +26,18 @@ public class PlayerBehaviour : MonoBehaviour {
     {
         rb = gameObject.GetComponent<Rigidbody>();
         layer = gameObject.layer;
+        lastShot = Time.time;
     }
 
 	// Update is called once per frame
 	void Update ()
     {
         //shoots if the player tries to shoot
-		if (Input.GetButtonDown ("Fire1")) {
+        if (Input.GetButton ("Fire1") && (Time.time > fireRate + lastShot)) {
 			GameObject thisShot;
 			thisShot = Instantiate(projectile, spawnPoint.position, spawnPoint.rotation) as GameObject;
             thisShot.layer = layer;
+            lastShot = Time.time;
 		}
 
         //Rotation controls
