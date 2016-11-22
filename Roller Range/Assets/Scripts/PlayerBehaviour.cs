062eb1501f54060b027d4a9340e5255abfbe470f
jthra037 <its_thrasher@live.ca>
2016-11-22 13:58:32 -0500

    Fix PlayerBehaviour conflicts

--- Roller Range/Assets/Scripts/PlayerBehaviour.cs
+++ Roller Range/Assets/Scripts/PlayerBehaviour.cs
@@ -3,41 +3,85 @@ using System.Collections;
 
 public class PlayerBehaviour : MonoBehaviour {
 
+    //Public links to things outside the script
     public GameObject projectile; //linked projectile prefab
-    public Transform spawnPoint; //Transform of bullet spawner
+	public GameObject projWall;
+	public Transform spawnPoint; //Transform of bullet spawner
     public float speed = 15;
     public Transform myCamera;
+	public int health = 20;
 
+    //Public vars for camera movement
     public float rotSpeedY = 20;
     public float rotSpeedX = 40;
     public bool yInvert = false;
     public float minRot = -70;
     public float maxRot = 70;
 
-    [SerializeField]
-    private float fireRate = 1f;
-
-    private float lastShot;
+    // Private vars for movement
     private Rigidbody rb;
     private Vector3 movement;
     private int layer;
+
+    // Private vars for weapons
+	private int wepIndex = 0;
+    private float pistolRoF = 1.0f;
+    private float nextShot;
+	private float tmgRoF = 0.15f;
+	[SerializeField] 
+	private float wallRoF = 0.5f;
+
+	private int combo = 0;
+	private bool runTimer = true;
+	private IEnumerator coroutine;
 
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
         layer = gameObject.layer;
-        lastShot = Time.time;
+        nextShot = Time.time;
+		coroutine = upgradeTimer ();
+		StartCoroutine (coroutine);
     }
 
 	// Update is called once per frame
 	void Update ()
     {
+
+		if (runTimer) {
+			StartCoroutine (coroutine);
+		}
+
+		if (Input.GetKey (KeyCode.Alpha1)) {
+			wepIndex = 0;
+			Debug.Log (wepIndex);
+		}
+		if (Input.GetKey (KeyCode.Alpha2) && combo > 0) {
+			wepIndex = 1;
+			Debug.Log (wepIndex);
+		}
+		if (Input.GetKey (KeyCode.Alpha3) && combo > 1) {
+			wepIndex = 2;
+			Debug.Log (wepIndex);
+		}
+
         //shoots if the player tries to shoot
-        if (Input.GetButton ("Fire1") && (Time.time > fireRate + lastShot)) {
-			GameObject thisShot;
-			thisShot = Instantiate(projectile, spawnPoint.position, spawnPoint.rotation) as GameObject;
-            thisShot.layer = layer;
-            lastShot = Time.time;
+		if (Input.GetButton ("Fire1")) {
+            switch (wepIndex)
+            {
+                case 0:
+                    shootPistol();
+                    break;
+                case 1:
+                    shootWalls();
+                    break;
+				case 2:
+					shootTMG();
+					break;
+                default:
+                    Debug.Log("Somehow didn't have a weapon equipped.. Change weapons!");
+                    break;
+            }
 		}
 
         //Rotation controls
@@ -52,8 +96,70 @@ public class PlayerBehaviour : MonoBehaviour {
         movement = (transform.forward * Input.GetAxisRaw("Horizontal") * -speed) + (transform.right * Input.GetAxisRaw("Vertical") * speed);
     }
 
+    void shootPistol()
+    {
+        if (Time.time >= nextShot)
+        {
+            GameObject thisShot;
+            thisShot = Instantiate(projectile, spawnPoint.position, spawnPoint.rotation) as GameObject;
+            thisShot.layer = layer;
+            nextShot = Time.time + pistolRoF;
+        }
+    }
+
+    void shootTMG()
+    {
+        if (Time.time >= nextShot)
+        {
+			GameObject thisShot;
+			thisShot = Instantiate(projectile, spawnPoint.position, spawnPoint.rotation) as GameObject;
+			thisShot.layer = layer;
+            nextShot = Time.time + tmgRoF;
+        }
+    }
+
+	void shootWalls()
+	{
+		if (Time.time >= nextShot)
+		{
+			RaycastHit hit;
+			GameObject thisShot;
+			thisShot = Instantiate(projectile, spawnPoint.position, spawnPoint.rotation) as GameObject;
+			thisShot.layer = layer;
+
+			if (Physics.Raycast(spawnPoint.position, spawnPoint.forward, out hit, Mathf.Infinity))
+			{
+				Instantiate (projWall, hit.point, spawnPoint.rotation);
+			}
+
+			nextShot = Time.time + wallRoF;
+		}
+	}
+
+	void hit()
+	{
+		StopCoroutine (coroutine);
+		runTimer = true;
+		coroutine = upgradeTimer ();
+		--health;
+		--combo;
+		combo = (combo < 0) ? 0 : combo;
+		wepIndex = (combo < wepIndex) ? combo : wepIndex;
+		Debug.Log ("Player hit");
+		Debug.Log ("Player health at: " + health);
+	}
+
     void FixedUpdate()
     {
         rb.velocity = movement;
     }
+
+	IEnumerator upgradeTimer()
+	{
+		runTimer = false;
+		yield return new WaitForSeconds (15);
+		++combo;
+		runTimer = true;
+		coroutine = upgradeTimer ();
+	}
 }
