0baa1b9662f04522b8af4d12e50014eec21813d8
jthra037 <its_thrasher@live.ca>
2016-11-24 22:19:45 -0500

    Add spawner check if player is in sight Add spawning code to game controller

--- Roller Range/Assets/Scripts/GameController.cs
+++ Roller Range/Assets/Scripts/GameController.cs
@@ -1,15 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameController : MonoBehaviour {
+	public bool running = true;
+	public float spawnRate = 3f;
+
+	private Spawner[] spawnPoints;
+	private int spawnIndex;
+	private Transform player;
+	private int level = 0;
+
+	[SerializeField]
+	private List<GameObject> creepPrefabs = new List<GameObject>(3);
 
 	// Use this for initialization
 	void Start () {
-	
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		spawnPoints = transform.GetComponentsInChildren<Spawner> ();
+
+		StartCoroutine (levelTimer ());
+		StartCoroutine (spawning ());
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
+	}
+
+	int getPrefab()
+	{
+		return 0;
+	}
+
+	IEnumerator levelTimer()
+	{
+		while (running) {
+			yield return new WaitForSeconds (15);
+			++level;
+		}
+	}
+
+	IEnumerator spawning()
+	{
+		int prefabIndex;
+
+		while (running) {
+			spawnIndex = ++spawnIndex % spawnPoints.Length;
+			while (spawnPoints [spawnIndex].canSeePlayer) {
+				spawnIndex = ++spawnIndex % spawnPoints.Length;
+			}
+
+
+			prefabIndex = getPrefab();
+			yield return new WaitForSeconds(spawnRate);
+		}
 	}
 }
